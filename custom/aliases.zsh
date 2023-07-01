33481958f09033c2e7c3b1dc8f4590da0f6c130b
SarathLUN <lun.sarath@gmail.com>
2023-07-01 23:45:20 +0700

    commit my customization 2023-07-01.

--- custom/aliases.zsh
+++ custom/aliases.zsh
@@ -1,8 +1,9 @@
 alias python=python3
 
-alias vim="nvim"
+alias vim=nvim
 
-alias pmo="cd /Users/sarath/Library/CloudStorage/OneDrive-SharedLibraries-EZECOM/IT\ Department\ -\ IT\ -\ PMO\ -\ IT\ -\ PMO/"
+alias pmo="cd /Users/sarath.lun/Library/CloudStorage/OneDrive-SharedLibraries-EZECOM/IT\ Department\ -\ IT\ -\ PMO\ -\ IT\ -\ PMO/"
 
-alias onedrive="cd /Users/sarath/Library/CloudStorage/OneDrive-EZECOM/"
+alias onedrive="cd /Users/sarath.lun/Library/CloudStorage/OneDrive-EZECOM/"
+
 
