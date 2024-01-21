9f63f206dcd4beae040ace8221657f3e64a2f432
SarathLUN <lun.sarath@gmail.com>
2024-01-21 21:58:28 +0700

    add alias to lazygit

--- custom/aliases.zsh
+++ custom/aliases.zsh
@@ -9,3 +9,5 @@ alias onedrive="cd $HOME/Library/CloudStorage/OneDrive-EZECOM/"
 alias udemy="cd /Users/sarath.lun/go-workspace/src/github.com/SarathLUN/udemy-building-modern-web-applications-with-go/"
 
 alias chatgpt="open https://chat.openai.com/"
+
+alias lg=lazygit
