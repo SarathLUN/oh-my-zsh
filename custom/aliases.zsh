79df4469da566a9d0931bec9d66a63a5e3a458c0
SarathLUN <lun.sarath@gmail.com>
2024-01-21 21:55:19 +0700

    add alias to chatGPT website

--- custom/aliases.zsh
+++ custom/aliases.zsh
@@ -8,3 +8,4 @@ alias onedrive="cd $HOME/Library/CloudStorage/OneDrive-EZECOM/"
 
 alias udemy="cd /Users/sarath.lun/go-workspace/src/github.com/SarathLUN/udemy-building-modern-web-applications-with-go/"
 
+alias chatgpt="open https://chat.openai.com/"
