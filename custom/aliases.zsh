947f0a0a8dfe55a93c8c64e56f87cf14f27ea0dd
SarathLUN <lun.sarath@gmail.com>
2024-03-03 18:46:51 +0700

    update path to add pmo and onedrive shortcut.

--- custom/aliases.zsh
+++ custom/aliases.zsh
@@ -2,12 +2,12 @@ alias python=python3
 
 alias vim=lvim
 
-alias pmo="cd $HOME/Library/CloudStorage/OneDrive-SharedLibraries-EZECOM/IT\ Department\ -\ IT\ -\ PMO\ -\ IT\ -\ PMO/"
+alias pmo='cd $HOME/Ezecom\ Co.,\ Ltd/IT\ Department\ -\ Project\ Management\ Office\ -\ Documents/'
 
-alias onedrive="cd $HOME/Library/CloudStorage/OneDrive-EZECOM/"
+alias onedrive='cd $HOME/Library/CloudStorage/OneDrive-EzecomCo.,Ltd/'
 
-alias udemy="cd /Users/sarath.lun/go-workspace/src/github.com/SarathLUN/udemy-building-modern-web-applications-with-go/"
+alias udemy='cd /Users/sarath.lun/go-workspace/src/github.com/SarathLUN/udemy-building-modern-web-applications-with-go/'
 
-alias chatgpt="open https://chat.openai.com/"
+alias chatgpt='open https://chat.openai.com/'
 
 alias lg=lazygit
--- custom/path.zsh
+++ custom/path.zsh
@@ -1,4 +1,4 @@
 export EDITOR=vim
 export PATH=$HOME/.cargo/bin:$PATH
 export PATH=$HOME/.local/bin:$PATH
-
+export PATH="/Applications/Alacritty.app/Contents/MacOS/alacritty":$PATH
