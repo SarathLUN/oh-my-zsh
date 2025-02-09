39b890edbf1ae3dd5ab17776a4f1d159ea6788a2
SarathLUN <lun.sarath@gmail.com>
2025-02-09 16:38:29 +0700

    use lazyvim instead of lunarvim

--- custom/aliases.zsh
+++ custom/aliases.zsh
@@ -3,17 +3,9 @@ alias python=python3
 #alias vim=lvim
 alias vim=nvim
 
-alias pmo='cd $HOME/Ezecom\ Co.,\ Ltd/IT\ Department\ -\ Project\ Management\ Office\ -\ Documents/'
-
-alias onedrive='cd $HOME/Library/CloudStorage/OneDrive-EzecomCo.,Ltd/'
-
 alias udemy='cd /Users/sarath.lun/go-workspace/src/github.com/SarathLUN/udemy-building-modern-web-applications-with-go/'
 
-alias chatgpt='open https://chat.openai.com/'
-
 alias lg=lazygit
-
-alias youtube='open https://youtube.com'
 
 alias smallscreen='cd ~/.config/alacritty && git checkout small-screen && cd -'
 alias bigscreen='cd ~/.config/alacritty && git checkout big-screen && cd -'
